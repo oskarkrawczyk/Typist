824fa3be5d949830122d34c7ce41e9e72394a624
Adam Schwartz <aschwartz@hubspot.com>
2015-01-12 21:58:20 -0500

    Add blinking cursor, selection, and color options

--- dist/helper.js
+++ dist/helper.js
@@ -1,13 +1,26 @@
 (function() {
   var load;
 
-  load = function() {
-    var el;
-    el = document.querySelector(INSTALL_OPTIONS.location);
+  load = function(options) {
+    var el, styles, stylesHTML;
+    el = document.querySelector(options.location);
     if (!el) {
       return;
     }
-    el.innerHTML = Typist.renderVariants(el.textContent, INSTALL_OPTIONS.variants, {
+    styles = document.createElement('div');
+    stylesHTML = '<styles>';
+    if (options.showSelection) {
+      stylesHTML += ".eager-typist-element .eager-typist-selected {\n  font-style: none;\n  color: #fff;\n  background: #000;\n  color: " + options.selectedColor + ";\n  background: " + options.selectedBackgroundColor + ";\n}";
+    } else {
+      stylesHTML += ".eager-typist-element .eager-typist-selected {\n  display: none;\n}";
+    }
+    if (options.showBlinkingCursor) {
+      stylesHTML += "@keyframes eager-typist-blinking-cursor {\n  0% { opacity: 1.0; }\n  50% { opacity: 0.0; }\n  100% { opacity: 1.0; }\n}\n\n@-webkit-keyframes eager-typist-blinking-cursor {\n  0% { opacity: 1.0; }\n  50% { opacity: 0.0; }\n  100% { opacity: 1.0; }\n}\n\n.eager-typist-element:after {\n  content: \"\00a0\";\n  display: inline-block;\n  margin-left: .125em;\n  width: .125em;\n  animation: eager-typist-blinking-cursor 1s step-start 0s infinite;\n  -webkit-animation: eager-typist-blinking-cursor 1s step-start 0s infinite;\n  background: #000;\n  background: " + options.cursorColor + ";\n}";
+    }
+    stylesHTML += '</styles>';
+    styles.innerHTML = stylesHTML;
+    document.body.appendChild(styles);
+    el.innerHTML = Typist.renderVariants(el.textContent, options.variants, {
       className: 'eager-typist-element'
     });
     return new Typist(el.querySelector('.eager-typist-element'), {
@@ -18,11 +31,13 @@
   };
 
   this.TypistInstallHelper = {
-    init: function() {
+    init: function(options) {
       if (document.readyState === 'LOADING') {
-        return window.addEventListener('DOMContentInteractive', load);
+        return window.addEventListener('DOMContentInteractive', function() {
+          return load(options);
+        });
       } else {
-        return load();
+        return load(options);
       }
     }
   };
--- helper.coffee
+++ helper.coffee
@@ -1,18 +1,70 @@
-load = ->
-  el = document.querySelector(INSTALL_OPTIONS.location)
+load = (options) ->
+  el = document.querySelector(options.location)
   return unless el
 
-  el.innerHTML = Typist.renderVariants el.textContent, INSTALL_OPTIONS.variants,
+  styles = document.createElement 'div'
+  stylesHTML = '<styles>'
+
+  if options.showSelection
+    stylesHTML += """
+      .eager-typist-element .eager-typist-selected {
+        font-style: none;
+        color: #fff;
+        background: #000;
+        color: #{ options.selectedColor };
+        background: #{ options.selectedBackgroundColor };
+      }
+    """
+
+  else
+    stylesHTML += """
+      .eager-typist-element .eager-typist-selected {
+        display: none;
+      }
+    """
+
+  if options.showBlinkingCursor
+    stylesHTML += """
+      @keyframes eager-typist-blinking-cursor {
+        0% { opacity: 1.0; }
+        50% { opacity: 0.0; }
+        100% { opacity: 1.0; }
+      }
+
+      @-webkit-keyframes eager-typist-blinking-cursor {
+        0% { opacity: 1.0; }
+        50% { opacity: 0.0; }
+        100% { opacity: 1.0; }
+      }
+
+      .eager-typist-element:after {
+        content: "\00a0";
+        display: inline-block;
+        margin-left: .125em;
+        width: .125em;
+        animation: eager-typist-blinking-cursor 1s step-start 0s infinite;
+        -webkit-animation: eager-typist-blinking-cursor 1s step-start 0s infinite;
+        background: #000;
+        background: #{ options.cursorColor };
+      }
+    """
+
+  stylesHTML += '</styles>'
+  styles.innerHTML = stylesHTML
+
+  document.body.appendChild styles
+
+  el.innerHTML = Typist.renderVariants el.textContent, options.variants,
     className: 'eager-typist-element'
 
   new Typist el.querySelector('.eager-typist-element'),
     letterInterval: 60
-    textInterval:   3000
+    textInterval: 3000
     selectClassName: 'eager-typist-selected'
 
 @TypistInstallHelper =
-  init: ->
+  init: (options) ->
     if document.readyState is 'LOADING'
-      window.addEventListener 'DOMContentInteractive', load
+      window.addEventListener 'DOMContentInteractive', -> load options
     else
-      load()
+      load options
