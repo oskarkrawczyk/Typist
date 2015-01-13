52e3f51269e37249cd96e1989ccc2325598371b1
Adam Schwartz <aschwartz@hubspot.com>
2015-01-12 22:04:55 -0500

    Compiled helper

--- dist/helper.js
+++ dist/helper.js
@@ -8,7 +8,7 @@
       return;
     }
     styles = document.createElement('div');
-    stylesHTML = '<styles>';
+    stylesHTML = '<style>';
     if (options.showSelection) {
       stylesHTML += ".eager-typist-element .eager-typist-selected {\n  font-style: none;\n  color: #fff;\n  background: #000;\n  color: " + options.selectedColor + ";\n  background: " + options.selectedBackgroundColor + ";\n}";
     } else {
@@ -17,7 +17,7 @@
     if (options.showBlinkingCursor) {
       stylesHTML += "@keyframes eager-typist-blinking-cursor {\n  0% { opacity: 1.0; }\n  50% { opacity: 0.0; }\n  100% { opacity: 1.0; }\n}\n\n@-webkit-keyframes eager-typist-blinking-cursor {\n  0% { opacity: 1.0; }\n  50% { opacity: 0.0; }\n  100% { opacity: 1.0; }\n}\n\n.eager-typist-element:after {\n  content: \"\00a0\";\n  display: inline-block;\n  margin-left: .125em;\n  width: .125em;\n  animation: eager-typist-blinking-cursor 1s step-start 0s infinite;\n  -webkit-animation: eager-typist-blinking-cursor 1s step-start 0s infinite;\n  background: #000;\n  background: " + options.cursorColor + ";\n}";
     }
-    stylesHTML += '</styles>';
+    stylesHTML += '</style>';
     styles.innerHTML = stylesHTML;
     document.body.appendChild(styles);
     el.innerHTML = Typist.renderVariants(el.textContent, options.variants, {
